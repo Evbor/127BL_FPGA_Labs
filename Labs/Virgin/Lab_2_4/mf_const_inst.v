56c0093f4e641b5335d87603f684a5046c7e29fe
Momo Belia Deviluke <evanbor@yahoo.com>
2018-02-02 16:59:16 -0800

    added the last few photos

---
+++ Labs/Virgin/Lab_2_4/mf_const_inst.v
@@ -0,0 +1,3 @@
+mf_const	mf_const_inst (
+	.result ( result_sig )
+	);
---
+++ Labs/Virgin/Lab_2_4/mf_divide_16_inst.vhd
@@ -0,0 +1,6 @@
+mf_divide_16_inst : mf_divide_16 PORT MAP (
+		denom	 => denom_sig,
+		numer	 => numer_sig,
+		quotient	 => quotient_sig,
+		remain	 => remain_sig
+	);
